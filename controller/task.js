9fd3158b1b6fc9e8166d894d4bd6982f5e9883b3
amitk9627 <ramitgzp56@gmail.com>
2025-04-21 23:10:34 +0530

    project setup mongodb and railway hosting setup

--- controller/task.js
+++ controller/task.js
@@ -1,10 +1,35 @@
 const Tasks = require("../model/task.js");
 
-const uploadGoogleSheet = async (req, res) => {};
-const addTask = async (req, res) => {};
-const getTask = async (req, res) => {};
-const updateTask = async (req, res) => {};
-const deleteTask = async (req, res) => {};
+const uploadGoogleSheet = async (req, res) => {
+  res.json({
+    status: true,
+    message: "uploaded successfully",
+  });
+};
+const addTask = async (req, res) => {
+  res.json({
+    status: true,
+    message: "uploaded successfully",
+  });
+};
+const getTask = async (req, res) => {
+  res.json({
+    status: true,
+    message: "uploaded successfully",
+  });
+};
+const updateTask = async (req, res) => {
+  res.json({
+    status: true,
+    message: "uploaded successfully",
+  });
+};
+const deleteTask = async (req, res) => {
+  res.json({
+    status: true,
+    message: "uploaded successfully",
+  });
+};
 
 module.exports = {
   uploadGoogleSheet,
